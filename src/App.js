d399c32e6f605e4a52c0dc4158dd98073853c0a4
Murtuza Chakkiwala <murtuza.chakkiwala@cignex.com>
2020-06-29 12:32:10 +0530

    sentry demo done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import { Helmet } from "react-helmet";
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({
+  dsn: "https://c3272801c4064ddca53820c63c2b89e2@o381216.ingest.sentry.io/5253700",
+  release: '1.0.1'
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+const handleClick = () => {
+  throw new Error('This is an error')
+}
+
+function Home() {
+  return <>
+    <Helmet>
+      <title>Home</title>
+    </Helmet>
+    <h1>This is Home page</h1>
+  </>;
+}
+
+function About() {
+  return <>
+    <Helmet>
+      <title>About</title>
+    </Helmet>
+    <h1>This is About page</h1>
+    <button onClick={() => { handleClick() }}>Break the world</button>
+  </>;
+}
+
+function Users() {
+  return <>
+    <Helmet>
+      <title>Users</title>
+    </Helmet>
+    <h1>This is Users page</h1>
+  </>;
 }
 
 export default App;
